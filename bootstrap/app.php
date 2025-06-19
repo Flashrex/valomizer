1dcd24e15f5aca5ab2879f51f28476442b91ad6c
Flashrex <flashrexwastaken@gmail.com>
2025-06-19 20:42:09 +0200

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -27,9 +28,9 @@ return Application::configure(basePath: dirname(__DIR__))
             TrackUniqueVisitor::class,
         ]);
 
-        $middleware->trustProxies([
-            env('TRUSTED_PROXY', ''),
-        ]);
+        $middleware->trustProxies(
+            explode(',', env('TRUSTED_PROXIES', '')) ?: []
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
